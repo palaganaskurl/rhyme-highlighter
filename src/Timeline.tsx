533f6bd3bc5a121b0830db583435a52a2839db4c
Kurl Angelo Palaganas <palaganaskurl@gmail.com>
2025-04-20 00:23:27 +0800

    feat: increase width

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -29,7 +29,7 @@ interface TimelineProps {
 
 export default function Timeline({ playerRef, lyrics }: TimelineProps) {
   const totalDuration = 148; // This should be derived from the actual length of the audio.
-  const timeLabelWidth = 100;
+  const timeLabelWidth = 200;
 
   const interval = 1000; // 1 second
   const trackCount = 2;
